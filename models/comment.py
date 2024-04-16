1df35629abd1dcd47e0dca9e8c97ea7509614ba5
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-04-16 17:36:31 +0100

    additional end points

--- models/comment.py
+++ models/comment.py
@@ -9,3 +9,5 @@ class CommentModel(db.Model, BaseModel):
     content = db.Column(db.Text, nullable=False)
 
     recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("UserModel", backref="comments")
