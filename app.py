431774ea4b33715321308bb5cf897160e841e2e9
Michael Broadbent~ <michael.broadbent95@gmail.com>
2025-03-20 15:19:58 +0000

    cors test

--- app.py
+++ app.py
@@ -14,35 +14,36 @@ from config.environment import db_URI
 
 from extensions import db, marsh, bcrypt
 
+
 def create_app():
 
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
 
-    CORS(app)
+    CORS(
+        app,
+        origins=[
+            "https://main--tasteful-trove.netlify.app/",
+        ],
+    )
 
     db.init_app(app)
     marsh.init_app(app)
     bcrypt.init_app(app)
 
-    
-
-
     @app.route("/", methods=["GET"])
     def home():
         return "Welcome to the Project 4 Backend!"
-
 
     @app.route("/hello", methods=["GET"])
     def hello():
         return "Hello, World!"
 
-
     with app.app_context():
         from seed import seed_database
-        seed_database()
 
+        seed_database()
 
     from controllers import recipes, users
 
@@ -50,5 +51,6 @@ def create_app():
     app.register_blueprint(users.router, url_prefix="/api")
 
     return app
+
 
 app = create_app()
