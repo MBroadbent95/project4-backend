557402699a7c365f98f212e4f4e63ee71330e392
Michael Broadbent~ <michael.broadbent95@gmail.com>
2025-03-20 13:36:28 +0000

    Added seeding workaround

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,6 +9,9 @@ from flask_bcrypt import Bcrypt
 from config.environment import db_URI
 
 from flask_cors import CORS
+
+from seed import seed_database
+
 
 app = Flask(__name__)
 
@@ -23,6 +26,16 @@ def hello():
     return "Hello, World!"
 
 
+@app.route("/seed", methods=["POST"])
+def seed_data():
+    try:
+        # Call your seeding function here
+        seed_database()
+        return jsonify({"message": "Database seeded successfully!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
 
 CORS(app)
@@ -32,6 +45,9 @@ db = SQLAlchemy(app)
 marsh = Marshmallow(app)
 
 bcrypt = Bcrypt(app)
+
+with app.app_context():
+    seed_database()
 
 
 from controllers import recipes, users
